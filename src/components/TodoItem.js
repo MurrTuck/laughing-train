f83776c08076c2f40fee44d472583d879646e1ce
MurrTuck <murrtuck@gmail.com>
2019-02-15 23:40:39 -0700

    Methods

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 
 export class TodoItem extends Component {
+
+    markComplete = (e) => {
+        console.log(this.props)
+    }
+
+
     render() {
         return (
             <div style={{ backgroundColor: '#f4f4f4' }}>
                 <p style={itemStyle}>
-                    <input type="checkbox" /> {' '}
+                    <input type="checkbox" onChange={this.markComplete} /> {' '}
                     {this.props.todo.title} </p>
                 <p style={durationStyle}>Estimated @ {this.props.todo.timeEstimate} Mins.
                 </p>
